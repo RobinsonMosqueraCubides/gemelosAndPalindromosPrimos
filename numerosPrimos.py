13bb97b73c74693e19cfd7bb9051185b62b9d650
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2023-12-19 14:16:19 -0500

    Mejora

--- numerosPrimos.py
+++ numerosPrimos.py
@@ -3,7 +3,7 @@ def gemelos(listaPrimo):
     for i in range(len(listaPrimo)-1):
         if listaPrimo[i]+2 == listaPrimo[i+1]:
             listaGemelo.append(listaPrimo[i])
-    print(f"los gemelos son {listaGemelo}")
+   # print(f"los gemelos son {listaGemelo}")
 
 
 def palindromos(listaPrimo):
@@ -12,18 +12,19 @@ def palindromos(listaPrimo):
         aux = str(listaPrimo[i])
         if listaPrimo[i] == int(aux[::-1]):
             listaPalindromo.append(listaPrimo[i])
-    print(f"los palindormos son {listaPalindromo}")
+  #  print(f"los palindormos son {listaPalindromo}")
 
 def calcularNumeroPrimo(lista):
     listaPrimo = []
     cont=0
     for i in lista:
-        for j in lista:
+        for j in range(2,int(i**0.5)+1):
            if i%j==0:
                cont+=1
-        if cont <2:
+        if cont <1:
             listaPrimo.append(i)
         cont=0
+    print(listaPrimo)
     gemelos(listaPrimo)
     palindromos(listaPrimo)
         
